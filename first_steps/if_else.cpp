b276ea44cce687b10ca48e0eeaf53d2e929cd222
Renan <renan.lowe@hotmail.com>
2023-07-18 18:25:13 -0300

    if & else if & else

---
+++ first_steps/if_else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int age;
+
+    std::cout << "Whats your age?" << std::endl;
+    std::cin >> age;
+
+    if(age < 18){
+        std::cout << "Muito novo" << std::endl;
+    }
+    else if(age >= 18 && age < 60){
+        std::cout << "Adulto" << std::endl;
+    }
+    else{
+        std::cout << "Idoso" << std::endl;
+    }
+
+}
