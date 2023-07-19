73f7b33334a765c4fc5260399dc90ba3aaf4b683
Renan <renan.lowe@hotmail.com>
2023-07-19 15:31:57 -0300

    passyby reference and passby value

---
+++ first_steps/passBy.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void passByValue(int value) { //usado quando voce quer apenas ler o valor da variavel
+    value = 100;  // Modifica apenas a copia local da variavel
+}
+
+void passByReference(int& value) { //usado para quando voce quer modificar o valor da variavel
+    value = 100;  // Modifica o valor da variavel original
+}
+
+int main() {
+    int x = 10;
+
+    passByValue(x);
+    std::cout << "Valor de x após passByValue: " << x << std::endl;  // Saída: 10 (valor original não foi alterado)
+
+    passByReference(x);
+    std::cout << "Valor de x após passByReference: " << x << std::endl;  // Saída: 100 (valor original foi modificado)
+
+    return 0;
+}
