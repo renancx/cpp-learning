139edd9d51ef08dd1607c05acb746d626a313f18
Renan <renan.lowe@hotmail.com>
2023-07-24 21:58:13 -0300

    exercice 3 and 4

--- pointers/exercices.cpp
+++ pointers/exercices.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//exercicio 1 =====================================================
 void q1(){
     int n1;
     int n2;
@@ -20,18 +21,62 @@ void q1(){
     };
 }
 
+//exercicio 2 =====================================================
 void q2(int vetor[]){
     for(int i=0; i<10; i++){
         std::cout << "Valor da posicao " << i << " eh -> " << vetor[i] << std::endl;
     }
 }
 
-int main(){
+//exercicio 3 =====================================================
+int q3(int vetor[]){
+    int aux;
+    for(int i=0; i<5; i++){
+        std::cout << "Valor da posicao " << i << std::endl;
+        std::cin >> vetor[i];
+        aux = 2 * vetor[i];
+        vetor[i] = aux;
+    }
 
+    std::cout << "Vetor final: " <<std::endl;
+    for(int j=0; j<5; j++){
+        std::cout << vetor[j] << std::endl;
+    }
+    return aux;
+}
+
+//exercicio 4 =====================================================
+int q4(int vetor[]){
+    int valor;
+    std::cout << "Digite o valor" << std::endl;
+    std::cin >> valor;
+
+    for(int i=0; i<5; i++){
+        vetor[i] = valor;
+    }
+
+    std::cout << "\nVetor: " << std::endl;
+    for(int j=0; j<5; j++){
+        std::cout << vetor[j] << std::endl;
+    }
+
+    return valor;
+}
+
+int main(){
+    
     int vetor[10] = {10, 23, 43, 88, 77, 66, 122, 543, 125, 100};
     int *ponteiro = &vetor[0]; 
 
+    int vetor_q3[5]; 
+    int *ponteiro_q3 = &vetor_q3[0];
+
     //q1();
     //q2(ponteiro);
+    //q3(ponteiro_q3);
 
+    int vetor_q4[5];
+    int *ponteiro_q4 = &vetor_q4[0];
+
+    q4(ponteiro_q4);
 }
