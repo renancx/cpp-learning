bb4269f0033dce629dd387c117c59f614522044a
Renan <renan.lowe@hotmail.com>
2023-07-25 18:01:18 -0300

    update size of array

--- pointers/pointers9.cpp
+++ pointers/pointers9.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int sumOfElements(int array[]){
+int sumOfElements(int array[], int size){
     int aux = 0;
-    for(int i=0; i<5; i++){
+    for(int i=0; i<size; i++){
         aux += array[i];
     }
     std::cout << "Soma dos elementos da array = " << aux << std::endl;
@@ -12,7 +12,8 @@ int sumOfElements(int array[]){
 int main(){
     int array[] = {1,2,3,4,5};
     int *p = array;
+    int size = sizeof(array)/sizeof(array[0]);
 
-    sumOfElements(p);
+    sumOfElements(p, size);
     //ou sumOfElements(array);
 }
