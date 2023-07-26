484612a32c771e6f7faabc94d5aab0bf79a831ed
Renan <renan.lowe@hotmail.com>
2023-07-26 20:12:19 -0300

    structs in vectors

---
+++ sigma/structs3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct funcionario {
+    std::string nome;
+    double salario;
+    int idade;
+};
+
+typedef struct funcionario Funcionario;
+
+int main(){
+    Funcionario func[3];
+}
