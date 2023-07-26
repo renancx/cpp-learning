264d952f6184121d44e332dbd8137ab1b49381a6
Renan <renan.lowe@hotmail.com>
2023-07-26 19:00:42 -0300

    update (auto example)

--- sigma/function_templates.cpp
+++ sigma/function_templates.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
-template <typename Teste>
+template <typename Teste, typename Outro>
 
-Teste max(Teste x, Teste y){
-    if (x > y){
-        return x;
-    }
-    return y;
+auto max(Teste x, Outro y){
+    return y < x ? x : y;
 }
 
 int main(){
-    std::cout << max(3, 5) << std::endl;
+    std::cout << max(3, 5.3) << std::endl;
     std::cout << max(3.0, 5.0) << std::endl;
     std::cout << max(3.0f, 5.0f) << std::endl;
     std::cout << max('a', 'b') << std::endl;
