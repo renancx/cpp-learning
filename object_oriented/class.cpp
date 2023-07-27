71d22b98b388b6225f11f3a4a7f69f752515f5f1
Renan <renan.lowe@hotmail.com>
2023-07-27 15:23:56 -0300

    class attributes example

---
+++ object_oriented/class.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Human{
+    public: 
+        std::string name;
+        std::string job;
+        int age;
+
+        void eat(){
+            std::cout << "Eating" << std::endl;
+        };
+
+        void sleep(){
+            std::cout << "Sleeping" << std::endl;
+        };
+};
+
+int main(){
+
+    Human human1; //human1 is an object
+
+    human1.name = "Renan";
+    human1.age = 22;
+    human1.job = "boss";
+
+    std::cout << human1.name << std::endl;
+    std::cout << human1.age << std::endl;
+    std::cout << human1.job << std::endl;
+
+    
+}
